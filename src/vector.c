121ee31524892f327717e76dda2042b4574ea98e
Ilia Munaev <imunaev-@c1r4p5.hive.fi>
2025-06-04 16:25:56 +0300

    Add: vector.c

---
+++ src/vector.c
@@ -0,0 +1,16 @@
+#include "minirt.h"
+
+/*
+Scenario: A tuple with w=0 is a vector
+Given a ← tuple(4.3, -4.2, 3.1, 0.0)
+Then a.x = 4.3
+And a.y = -4.2
+And a.z = 3.1
+And a.w = 0.0
+And a is not a point
+And a is a vector
+
+Scenario: vector() creates tuples with w=0
+Given v ← vector(4, -4, 3)
+Then v = tuple(4, -4, 3, 0)
+*/
