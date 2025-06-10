9ab1b68a8c7b9417c4a4703df009a9a9ca3b2c3c
Laurens Haas <lhaas@c2r5p3.hive.fi>
2025-06-10 18:08:59 +0300

    Update: test_translate()

--- tests/test_translate.c
+++ tests/test_translate.c
@@ -2,14 +2,16 @@
 
 int main(void)
 {
-    t_tuple vector = {1, 2, 3, 1};
+    t_tuple point = {-3, 4, 5, 1};
     t_tuple result;
-    t_tuple translation = {-1, 1, 3, 1};
+    t_tuple translation = {5, -3, 2, 1};
     t_matrix matrix;
+    t_matrix inverted_matrix;
 
     translate(&matrix, translation);
-    print_matrix(matrix);
+    invert_matrix(&inverted_matrix, matrix);
+    print_matrix(inverted_matrix);
      printf("\n");
-    mult_matrix_by_tuple(&result, matrix, vector);
+    mult_matrix_by_tuple(&result, inverted_matrix, point);
     printf("\n translated point is= x: %f, y: %f, z: %f, w: %f\n", result.x, result.y, result.z, result.w);
 }
