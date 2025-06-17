dd0ccc545b36a141f638b51e12d08de6cd406247
Ilia Munaev <imunaev-@c1r5p21.hive.fi>
2025-06-17 15:59:27 +0300

    Update: s_primitive struct

--- include/primitives.h
+++ include/primitives.h
@@ -7,7 +7,15 @@
 typedef struct s_primitive
 {
     int         id;
-    t_tuple     position;
+    char        char_id; // aka "A" for amb_light or "C" for cam
+    t_tuple     position;  // aka t_tuple coordinates
+    t_tuple     norm_vector; //  for cam, plane, cylinder
+	float       ratio;      // for amb_light
+    float       brightness;  // for  light, 
+    t_tuple     color;  
+    float       diameter;  // for sphere, cylinder
+    float       height;   // for cylinder
+    float		fov;    // for cam 
     t_matrix    matrix;
 }   t_primitive;
 
