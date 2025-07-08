98a2307480ff83d454193b313eb113cf91e5f5c6
Ilia Munaev <imunaev-@c1r5p15.hive.fi>
2025-07-08 15:09:57 +0300

    Refactor: mouse_loop.c according Norm code style

--- src/loop/mouse_loop.c
+++ src/loop/mouse_loop.c
@@ -9,36 +9,41 @@ void	handle_mouse_move(double x, double y, void *param)
 	world->mouse_y = y;
 }
 
-void	handle_mouse_click(mouse_key_t button,
-		action_t action,
-		modifier_key_t mods,
-		void *param)
+static bool	is_left_mouse_click(mouse_key_t button, action_t action)
+{
+	return (action == MLX_PRESS && button == MLX_MOUSE_BUTTON_LEFT);
+}
+
+static void	select_hit_object(t_rt *world, t_ray *ray)
+{
+	int	i;
+
+	i = 0;
+	world->selected_primitive_index = -1;
+	while (i < world->obj_counted)
+	{
+		if (&world->primitives_list[i] == ray->hit.object)
+		{
+			world->selected_primitive_index = i;
+			break ;
+		}
+		i++;
+	}
+}
+
+void	handle_mouse_click(mouse_key_t button, action_t action,
+						modifier_key_t mods, void *param)
 {
 	t_rt	*world;
 	t_ray	ray;
-	int		index;
-	int		i;
 
 	world = (t_rt *)param;
-	if (action != MLX_PRESS || button != MLX_MOUSE_BUTTON_LEFT)
+	(void)mods;
+	if (!is_left_mouse_click(button, action))
 		return ;
 	ray_for_pixel(&ray, &world->cam, world->mouse_x, world->mouse_y);
 	get_ray_intersections(&ray, world);
 	get_hit(&ray);
 	if (ray.is_hit)
-	{
-		index = -1;
-		i = 0;
-		while (i < world->obj_counted)
-		{
-			if (&world->primitives_list[i] == ray.hit.object)
-			{
-				index = i;
-				break ;
-			}
-			i++;
-		}
-		world->selected_primitive_index = index;
-		printf("DEBUG: Clicked and selected primitive #%d \n", index);
-	}
+		select_hit_object(world, &ray);
 }
