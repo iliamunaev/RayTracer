cc4784c4f41af37c8ab16118dc744a8ba73ecfe2
Ilia Munaev <imunaev-@c1r5p15.hive.fi>
2025-07-09 16:33:14 +0300

    Reafactor: minirt.h according to Norm code style

--- src/minirt.c
+++ src/minirt.c
@@ -1,29 +1,21 @@
 #include "minirt.h"
 
-
 int	main(int argc, char **argv)
 {
-    (void)argc;
-    t_rt world;
+	t_rt	world;
 
-    init_world(&world);
-    setup_mlx(&world);
-
-
-    if (parse(argv[1], &world) != EXIT_SUCCESS)
-    {
-        mlx_terminate(world.mlx);
-        return (EXIT_FAILURE);
-
-    }
-    render(&world);
-
-    
-    mlx_key_hook(world.mlx, world_loop, &world);
-    mlx_mouse_hook(world.mlx, handle_mouse_click, &world);    
-    mlx_cursor_hook(world.mlx, handle_mouse_move, &world);
-    mlx_loop(world.mlx);
-    mlx_terminate(world.mlx);
-
-    return (EXIT_SUCCESS);
+	init_world(&world);
+	setup_mlx(&world);
+	if (parse(argv[1], &world) != EXIT_SUCCESS)
+	{
+		mlx_terminate(world.mlx);
+		return (EXIT_FAILURE);
+	}
+	render(&world);
+	mlx_key_hook(world.mlx, world_loop, &world);
+	mlx_mouse_hook(world.mlx, handle_mouse_click, &world);
+	mlx_cursor_hook(world.mlx, handle_mouse_move, &world);
+	mlx_loop(world.mlx);
+	mlx_terminate(world.mlx);
+	return (EXIT_SUCCESS);
 }
