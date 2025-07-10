a2867bf266371c64f1b05ab92d661cb682fb85f5
Laurens Haas <lhaas@c1r5p17.hive.fi>
2025-07-10 14:01:16 +0300

    Refactor: lighting, ray

--- src/lighting/reflection_refraction_shadow_checker.c
+++ src/lighting/reflection_refraction_shadow_checker.c
@@ -24,62 +24,6 @@ void	create_checkerboard(t_tuple *effective_color, t_comps *comps,
 	}
 }
 
-void	find_diffuse_secular(t_tuple *diffuse, t_tuple *specular,
-		t_comps *comps, t_tuple *eff_col, t_rt *world)
-{
-	float	l_dot;
-	float	reflect_dot_eye;
-	t_tuple	light_v;
-	t_tuple	reflect_v;
-	t_tuple	negated_light_v;
-
-	sub_tuples(&light_v, world->light.position, comps->position);
-	normalize_vector(&light_v);
-	l_dot = dot_product(light_v, comps->v_normal);
-	if (l_dot >= 0)
-	{
-		create_color(diffuse, eff_col->r, eff_col->g, eff_col->b);
-		mult_tuple(diffuse, (comps->object->material.diffuse * l_dot));
-		create_vector(&negated_light_v, light_v.x, light_v.y, light_v.z);
-		negate_tuple(&negated_light_v);
-		reflect_vec(&reflect_v, negated_light_v, comps->v_normal);
-		reflect_dot_eye = dot_product(reflect_v, comps->v_eye);
-		if (reflect_dot_eye <= 0)
-			return ;
-		else
-		{
-			create_color(specular, eff_col->r, eff_col->g, eff_col->b);
-			mult_tuple(specular, (world->light.brightness
-					* comps->object->material.specular * pow(reflect_dot_eye,
-						comps->object->material.shininess)));
-		}
-	}
-}
-
-void	lighting(t_tuple *color, t_comps *comps, t_rt *world, bool in_shadow)
-{
-	t_tuple	effective_color;
-	t_tuple	ambient;
-	t_tuple	diffuse;
-	t_tuple	specular;
-
-	if (comps->object->bonus_type == CHECKER)
-		create_checkerboard(&effective_color, comps, world->light);
-	else
-		mult_colors(&effective_color, comps->object->color,
-			world->light.color_component);
-	mult_colors(&ambient, effective_color, world->amb.amb_component);
-	if (in_shadow == true && comps->shadow_factor < 0.04)
-		return (create_color(color, ambient.r, ambient.g, ambient.b));
-	create_color(&diffuse, 0, 0, 0);
-	create_color(&specular, 0, 0, 0);
-	find_diffuse_secular(&diffuse, &specular, comps, &effective_color, world);
-	add_tuples(color, ambient, diffuse);
-	add_tuples(color, *color, specular);
-	if (in_shadow == true && comps->shadow_factor > 0.04)
-		mult_color_scal(color, comps->shadow_factor);
-}
-
 bool	check_shadow(t_rt *world, t_tuple point, t_comps *comps)
 {
 	t_tuple	v_shadow;
@@ -125,8 +69,7 @@ void	refracted_color(t_tuple *refract_col, t_rt *world, t_comps *comps,
 	create_vector(&dir_t1, comps->v_normal.x, comps->v_normal.y,
 		comps->v_normal.z);
 	mult_color_scal(&dir_t1, (refrac.n_ratio * refrac.cos_i - refrac.cos_t));
-	create_vector(&dir_t2, comps->v_eye.x, comps->v_eye.y,
-		comps->v_eye.z);
+	create_vector(&dir_t2, comps->v_eye.x, comps->v_eye.y, comps->v_eye.z);
 	mult_color_scal(&dir_t2, refrac.n_ratio);
 	sub_tuples(&refract_direction, dir_t1, dir_t2);
 	create_ray(&refract_ray, comps->under_pos, refract_direction);
@@ -173,42 +116,4 @@ void	reflection(t_tuple *reflect_col, t_rt *world, t_comps *comps,
 	create_ray(&reflect_ray, comps->over_pos, comps->v_reflection);
 	color_at(reflect_col, world, &reflect_ray, remaining_depth - 1);
 	mult_color_scal(reflect_col, comps->object->material.reflection);
-}
-
-void	shade_hit(t_tuple *color, t_rt *world, t_comps *comps,
-		uint8_t remaining_depth)
-{
-	bool	is_shaded;
-	t_tuple	reflect_col;
-	t_tuple	refract_col;
-	float	reflectance;
-
-	create_color(&reflect_col, 0, 0, 0);
-	create_color(&refract_col, 0, 0, 0);
-	is_shaded = check_shadow(world, comps->over_pos, comps);
-	lighting(color, comps, world, is_shaded);
-	reflection(&reflect_col, world, comps, remaining_depth);
-	refracted_color(&refract_col, world, comps, remaining_depth);
-	if (comps->object->material.reflection > 0
-		&& comps->object->material.transparency > 0)
-	{
-		schlick(&reflectance, comps);
-		mult_color_scal(&reflect_col, reflectance);
-		mult_color_scal(&refract_col, 1.0 - reflectance);
-	}
-	add_colors(color, *color, reflect_col);
-	add_colors(color, *color, refract_col);
-}
-
-void	color_at(t_tuple *color, t_rt *world, t_ray *ray,
-		uint8_t remaining_depth)
-{
-	t_comps	comps;
-
-	get_ray_intersections(ray, world);
-	get_hit(ray);
-	if (ray->is_hit == false)
-		return ;
-	precompute_values(&comps, ray);
-	shade_hit(color, world, &comps, remaining_depth);
 }
---
+++ src/ray/ray_math.c
@@ -0,0 +1,23 @@
+#include "minirt.h"
+
+
+void	ray_transform(t_ray *ray, t_matrix matrix)
+{
+	mult_matrix_by_tuple(&ray->origin, matrix, ray->origin);
+	mult_matrix_by_tuple(&ray->direction, matrix, ray->direction);
+}
+
+void	create_ray(t_ray *ray, const t_tuple point, const t_tuple vector)
+{
+	ray->origin = point;
+	ray->direction = vector;
+	ray->is_hit = false;
+}
+
+void	get_position(t_tuple *position, t_ray *ray, float time)
+{
+	position->x = ray->origin.x + ray->direction.x * time;
+	position->y = ray->origin.y + ray->direction.y * time;
+	position->z = ray->origin.z + ray->direction.z * time;
+	position->w = ray->origin.w + ray->direction.w * time;
+}
